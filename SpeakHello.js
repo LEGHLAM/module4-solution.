f6acc454e8c71292fc4c27b1611e4d2f26842f5d
LEGHLAM <67144764+LEGHLAM@users.noreply.github.com>
2020-07-23 11:35:52 +0200

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,12 @@
+
+(function(window){
+var helloSpeaker={};
+var speak = "Hello";
+
+
+ helloSpeaker.speak = function (name) {
+  console.log(speak + " " + name);
+}
+window.helloSpeaker=helloSpeaker
+})(window);
+
